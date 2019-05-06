5e3be16e2dd315c6589ffbdc4df204eb68fbe995
duoc.nguyen <duoc.nguyen@asiantech.vn>
2019-05-06 15:02:14 +0700

    update code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    if (localStorage.authToken) {
+        authInfo();
+    }
     $('.btn-discover').click(function(e) {
         e.preventDefault();
         $('html,body').animate(
@@ -32,16 +35,13 @@ $(document).ready(function() {
 
     window.fbAsyncInit = function() {
         FB.init({
-            appId: '406500586870239',
+            appId: '2423738151191635',
             cookie: true,
             xfbml: true,
             version: 'v3.3'
         });
 
         FB.AppEvents.logPageView();
-        FB.getLoginStatus(function(response) {
-            console.log(response);
-        });
     };
 
     (function(d, s, id) {
@@ -55,4 +55,63 @@ $(document).ready(function() {
         js.src = 'https://connect.facebook.net/en_US/sdk.js';
         fjs.parentNode.insertBefore(js, fjs);
     })(document, 'script', 'facebook-jssdk');
+
+    $('#btn-logout').click(() => {
+        localStorage.clear();
+        location.reload(true);
+    });
+
+    $('#btn-profile-setting').click(e => {});
 });
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            $.post({
+                url: '/api/facebook/profile',
+                data: {
+                    access_token: response.authResponse.accessToken
+                },
+                success: resp => {
+                    localStorage.authToken = JSON.stringify(resp.token);
+                    localStorage.authInfo = JSON.stringify(resp.user);
+                    authInfo();
+                    $('#modal-login').modal('hide');
+                    if (resp.is_new) {
+                        $('#modal-new-user-setting').modal();
+                    }
+                },
+                error: resp => {
+                    alert('Internal server error! Please try again later.');
+                }
+            });
+        }
+    });
+}
+
+function authInfo() {
+    userInfo = JSON.parse(localStorage.authInfo);
+    $('#auth-info')
+        .html(
+            `<a href=''>
+            Hi, ${userInfo.username}
+            <img class='rounded-circle' src='${
+                userInfo.profile_picture
+            }' alt='user-img'>
+        </a>
+        <ul class="dropdown" id="auth-logout">
+            <li class="dropdown-item"><a href="">Profile</a></li>
+            <li class="dropdown-item"><a href="">Logout</a></li>
+        </ul>`
+        )
+        .show();
+}
+
+function requestSetting() {
+    token = JSON.parse(localStorage.authToken);
+    $.ajaxSetup({
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        }
+    });
+}
--- datingapp/models/user.ts
+++ datingapp/models/user.ts
@@ -1,4 +1,4 @@
-import { Model, INTEGER, STRING, TEXT } from 'sequelize';
+import { Model, INTEGER, STRING, TEXT, TIME, SMALLINT } from 'sequelize';
 import DB from '../util/db';
 
 const sequelize = DB;
@@ -14,10 +14,42 @@ User.init({
             type: STRING,
             allowNull: false,
             unique: true,
-
         },
         nickname: {
             type: STRING
+        },
+        profile_picture: {
+            type: STRING,
+        },
+        age: {
+            type: SMALLINT
+        },
+        gender: {
+            type: STRING
+        },
+        income_level: {
+            type: STRING
+        },
+        location_id: {
+            type: INTEGER
+        },
+        address: {
+            type: STRING
+        },
+        occupation: {
+            type: STRING
+        },
+        ethnic: {
+            type: STRING
+        },
+        created_at: {
+            type: TIME
+        },
+        updated_at: {
+            type: TIME
+        },
+        deleted_at: {
+            type: TIME
         }
     },
     { sequelize, modelName: 'users' }
