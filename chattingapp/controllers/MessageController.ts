c0747671c88b7798679b45b4d6fe4fc905d7f35a
ml-tam-nguyen4 <tam.nguyen4-fr@asiantech.vn>
2019-05-21 14:17:35 +0700

    update api

--- chattingapp/controllers/MessageController.ts
+++ chattingapp/controllers/MessageController.ts
@@ -19,14 +19,13 @@ export class MessageController {
         const decoded = Utils.jwtVerify(token);
         const contentMessage = req.body.message;
         try {
-            const user = await User.findOne({ id: decoded.id }).select('_id');
-            const room = await Room.findOne({ _id: roomID, 'user_rooms._id': user._id });
+            const room = await Room.findOne({ _id: roomID, 'user_rooms': decoded.id });
             if (!room) {
                 return Http.NotFoundResponse(res, { message: 'Room is not found' });
             }
             const message = new Message({
                 message: contentMessage,
-                user_id: user._id,
+                user_id: decoded.id,
                 room_id: roomID
             });
             message.save();
@@ -46,8 +45,7 @@ export class MessageController {
         const limit = req.query.limit;
         const sinceID = req.query.since_id;
         try {
-            const user = await User.findOne({ id: decoded.id }).select('_id');
-            const room = await Room.findOne({ _id: roomID, 'user_rooms._id': user._id });
+            const room = await Room.findOne({ _id: roomID, 'user_rooms': decoded.id });
             if (!room) {
                 return Http.NotFoundResponse(res, { message: 'Room is not found' });
             }
@@ -57,57 +55,12 @@ export class MessageController {
             } else {
                 cond = { id: { $lt: parseInt(sinceID) } };
             }
-            const messages = await Message.aggregate([
-                {
-                    $match: {
-                        $and: [
-                            { room_id: Types.ObjectId(roomID) },
-                            cond
-                        ]
-                    }
-                },
-                {
-                    $project: {
-                        message: 1,
-                        user_id: 1,
-                        id: 1,
-                        created_at: 1,
-                    }
-                },
-                {
-                    $lookup:
-                    {
-                        from: 'users',
-                        localField: 'user_id',
-                        foreignField: '_id',
-                        as: 'user_info'
-                    }
-                },
-                {
-                    $project: {
-                        message: 1,
-                        created_at: 1,
-                        id: 1,
-                        'user_info._id': 1,
-                        'user_info.nickname': 1,
-                        'user_info.avatar_url': {
-                            $ifNull: [
-                                '$avatar_url',
-                                config.get('dating_app.default_user_avatar')
-                            ]
-                        }
-                    }
-                },
-                {
-                    $unwind: '$user_info'
-                },
-                {
-                    $limit: parseInt(limit)
-                },
-                {
-                    $sort: { created_at: -1 }
-                }
-            ]);
+            const messages = await Message.find({
+                $and: [
+                    { room_id: Types.ObjectId(roomID) },
+                    cond
+                ]
+            }).limit(parseInt(limit)).sort({ created_at: -1 });
             return Http.SuccessResponse(res, messages);
         } catch (error) {
             return Http.InternalServerResponse(res);
